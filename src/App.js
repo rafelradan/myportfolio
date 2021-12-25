ffb38e08096f64d638211bc50733a6396001d166
Rafael Nascimento <rafaelradan76@gmail.com>
2021-12-25 06:28:28 -0300

    Add just the top

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './StyleApp.scss'
+import Myphoto from './img/Fotopefil.png'
+import Github from './img/github-50.png'
+import Linkedin from './img/linkedin-50.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="left-side">
+          <div className="back-img">
+            <img className="myphoto" src={Myphoto} alt="Rafael Nascimento" />
+          </div>
+
+          <div className="back-text">
+            <div className="name">
+              <span className="span1">Rafael</span>
+              <span className="span2">Nascimento</span>
+            </div>
+          </div>
+          <div className="description">
+            <span>Front-end developer</span>
+          </div>
+          <div className="links">
+            <img className="img" src={Github} alt="Github"></img>
+            <img className="img" src={Linkedin} alt="Linkedin"></img>
+          </div>
+        </div>
+        <div className="right-side">
+          <div className="background"></div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
