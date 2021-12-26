41749ad065da334ee2579e1f0eafc829172d09b9
Rafael Nascimento <rafaelradan76@gmail.com>
2021-12-26 20:16:38 -0300

    Added the first two projects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Myphoto from './img/Fotopefil.png'
 import Github from './img/github-50.png'
 import Linkedin from './img/linkedin-50.png'
 
+import MyPortfolio from './img/ProjectMyPortfolio01.png'
+import Filmaria from './img/ProjectFilmariaRdPortfolio01.png'
+import DevMediaTest from './img/ProjectTestDevMediaPortfolio01.png'
+import CadUser from './img/ProjectCadUserPortfolio01.png'
+
 function App() {
   return (
     <div className="App">
@@ -42,9 +47,70 @@ function App() {
             <span> HTML | CSS | Java Script | ReactJS</span>
           </div>
         </div>
-
+        {/* Start -> Projects */}
         <div className="projetos">
           <h3>Projects</h3>
+          <section className="projects">
+            {/* Project 01 */}
+            <div className="individual-project">
+              <div className="title">
+                <h5>Portfólio</h5>
+              </div>
+              <div className="content">
+                <div className="imgProject">
+                  <img src={MyPortfolio} alt="MyPortfolio" />
+                </div>
+
+                <div className="paragraph">
+                  <p>
+                    Esse é o meu Portfólio, projeto Front-end criado usando
+                    React. Aqui estão alguns dos projetos desenvolvidos por mim.
+                    Meu foco principal é Front-end.
+                  </p>
+                </div>
+              </div>
+
+              <button className="btn-acessar">
+                <a
+                  href="https://rafaelnascimento.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Acessar
+                </a>
+              </button>
+            </div>
+            {/* Project 01 end*/}
+            {/* Project 02 */}
+            <div className="individual-project">
+              <div className="content">
+                <div className="imgProject">
+                  <img src={Filmaria} alt="MyPortfolio" />
+                </div>
+
+                <div className="paragraph">
+                  <p>
+                    Esse é um projeto também desenvolvido em React e totalmente
+                    resposivo, nesse caso o projeto consome uma API sobre
+                    filmes, acessando Nome, Capa e Sinopse do filme. O site tem
+                    três rotas, a Home, Favoritos e uma para pesquisar por
+                    filmes no YouTube.
+                  </p>
+                </div>
+              </div>
+
+              <button className="btn-acessar">
+                <a
+                  href="https://rafaelnascimento.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Acessar
+                </a>
+              </button>
+            </div>
+            {/* Project 02 end*/}
+          </section>
         </div>
 
         <div className="sobre">
